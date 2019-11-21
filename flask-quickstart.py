a8f40ef4744cc415b70852127edce9f255cca708
Colin Thomas Kraczkowsky <Home@colins-mbp-2.corp.lookout.com>
2019-11-21 13:02:59 -0800

    Edited the main module.

--- flask-quickstart.py
+++ flask-quickstart.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/simple-app')
 def home():
-    return '<p style="color: red">Hiyee!</p>'
+    return '<h1 style="color: red">Hiyee!</h1>'
 
 if __name__ == 'main':
     app.run()
